e3ec4d416ad116dcd86dd9ce41ea5ae763fb3673
Kvetuse Husakova <42646031+Yushikuni@users.noreply.github.com>
2023-09-30 19:39:43 +0200

    update shooting system
    Just update shooting system with add tags, fire timer set from 0.1f to 0.5f also add funcionality for nuke.
    Change product name (SpaceWar!_UnityCopy) and company (NonE)

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject nukePrefab;
     public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+    public Vector3 nukeOffset = new Vector3(0,0.5f, 0);
 
-    public float fireTimer = 0.1f;
+    public float fireTimer = 0.5f;
     float cooldownTimer = 0f;
+    float nukeCooldownTimer = 500f;
 
     // Update is called once per frame
     void Update()
     {
         cooldownTimer -= Time.deltaTime;
+        nukeCooldownTimer -= Time.deltaTime;
         if(Input.GetButton("Fire1") && cooldownTimer <= 0)
         {
             Debug.Log("kabóóm!");
@@ -21,8 +25,16 @@ public class PlayerShooting : MonoBehaviour
 
             Vector3 offset = transform.rotation * bulletOffset;
             Instantiate(bulletPrefab, transform.position + offset, transform.rotation);
-            // GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, transform.position, transform.rotation);
-            //bulletGO.layer = gameObject.layer;
+            GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bulletGO.layer = gameObject.layer;
+        }
+       
+        if(Input.GetButton("Fire2") && nukeCooldownTimer <= 0) 
+        {
+            Debug.Log("Nuke release");
+            nukeCooldownTimer = fireTimer;
+
+            Instantiate(nukePrefab, transform.position + (transform.rotation * nukeOffset), transform.rotation);
         }
     }
 }
